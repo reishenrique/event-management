d3f069fc5b26888f638754888b18ed46de82ee12
Henrique Reis <contatohenriquereis@gmail.com>
2022-11-07 15:40:47 -0300

    authentication middleware

---
+++ src/middleware/authMiddleware.ts
@@ -0,0 +1,30 @@
+import jwt  from 'jsonwebtoken';
+import { User } from '../models/User';
+import { UnauthorizedError } from './../helpers/api-errors';
+import { Request, Response, NextFunction } from 'express';
+import 'dotenv/config';
+
+type JwtPayload = {
+    id: string
+}
+
+export const authMiddleware = async (req: Request, res: Response, _next: NextFunction) => {
+    const { authorization } = req.headers;
+    if(!authorization) {
+        throw new UnauthorizedError('Não autorizado');
+    }
+
+    const token = authorization.split(' ')[1];
+    
+    const { id } = jwt.verify(token, process.env.JWT_PASSWORD ?? '') as JwtPayload
+
+    const userId = await User.findOne({ _id: id });
+
+    if(!userId) {
+        throw new UnauthorizedError('Usuário não foi encontrado no banco de dados')
+    }
+
+    const { name, email } = userId;
+
+    return res.status(200).json({ name, email, id });
+}
