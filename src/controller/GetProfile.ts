9c85681cc810ed575063be2ec11358f4e3ff6c80
Henrique Reis <contatohenriquereis@gmail.com>
2022-11-07 15:41:56 -0300

    get profile route using authentication middleware

--- src/controller/GetProfile.ts
+++ src/controller/GetProfile.ts
@@ -1,33 +1,10 @@
+import { authMiddleware } from './../middleware/authMiddleware';
 import { Request, Response, Router } from 'express'
-import { User } from '../models/User'
-import { UnauthorizedError } from '../helpers/api-errors';
-import jwt from 'jsonwebtoken';
-import 'dotenv/config';
-
-type JwtPayload = {
-    id: string
-}
 
 const getProfile = Router();
 
-getProfile.get('/', async (req: Request, res: Response) => {
-    const { authorization } = req.headers
-    if(!authorization) {
-        throw new UnauthorizedError('Não autorizado')
-    }
-
-    const token = authorization.split(' ')[1]
-    const { id } = jwt.verify(token, process.env.JWT_PASSWORD ?? '') as JwtPayload
-
-    const userId = await User.findOne({ _id: id })
-
-    if(!userId) {
-        throw new UnauthorizedError('Não autorizado')
-    }
-
-    const { name, email } = userId
-
-    return res.status(200).json({name, email, id})
+getProfile.get('/', async (_req: Request, res: Response) => {
+        return res.json({ authMiddleware });
 })
 
-export { getProfile }
+export { getProfile };
